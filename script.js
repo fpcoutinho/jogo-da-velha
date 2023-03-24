1783fad363b555954e6db72a007ddeb66e652710
fpcoutinho <fpcoutinho5@gmail.com>
2023-03-24 19:20:49 -0300

    hover funcionando. o app funciona, só falta deixar o player escolher o simbolo.

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
+const tabuleiro = document.querySelectorAll(".casa");
+
+//enf game modal
+const modalNovoJogo = document.querySelector(".mensagem");
+const mensagemVencedor = document.querySelector(".mensagem-texto");
+
+//placares
 const placarX = document.querySelector(".placar-jogador-x");
 const placarO = document.querySelector(".placar-jogador-o");
-const bannerX = document.querySelector(".jogador-x");
-const bannerO = document.querySelector(".jogador-o");
-const tabuleiro = document.querySelectorAll(".casa");
+
+//botões
 const reiniciar = document.querySelector(".btn.reiniciar");
+const jogarNovamente = document.querySelector(".btn.jogar");
 const fechar = document.querySelector(".btn.fechar");
-const modalNovoJogo = document.querySelector(".mensagem");
+
+let playerAtivo = "x";
 
 tabuleiro.forEach((casa) => {
   casa.addEventListener("click", () => {
-    casa.classList.add("marcado", "x");
+    if (casa.classList.contains("marcado")) return;
+    casa.classList.add("marcado", playerAtivo);
+    mudarJogador();
     checkVencedor();
+    checkEmpate();
   });
+  casa.addEventListener(
+    "mouseenter",
+    function (event) {
+      if (!casa.classList.contains("marcado")) {
+        if (playerAtivo === "x") {
+          event.target.style.backgroundImage = "url(recursos/x.png)";
+        } else {
+          event.target.style.backgroundImage = "url(recursos/o.png)";
+        }
+      }
+    },
+    false
+  );
+  casa.addEventListener(
+    "mouseleave",
+    function (event) {
+      event.target.style.backgroundImage = "";
+    },
+    false
+  );
+});
+
+reiniciar.addEventListener("click", () => {
+  placarO.innerHTML = 0;
+  placarX.innerHTML = 0;
+  limparTabuleiro();
 });
 
 fechar.addEventListener("click", () => {
-  toggleMensagem();
+  sair();
+});
+
+jogarNovamente.addEventListener("click", () => {
+  jogarDeNovo();
 });
 
 function checkVencedor() {
@@ -32,27 +73,60 @@ function checkVencedor() {
 
   combs.forEach((comb) => {
     if (
-      (tabuleiro[comb[0]].classList.contains("x") &&
-        tabuleiro[comb[1]].classList.contains("x") &&
-        tabuleiro[comb[2]].classList.contains("x")) ||
-      (tabuleiro[comb[0]].classList.contains("o") &&
-        tabuleiro[comb[1]].classList.contains("o") &&
-        tabuleiro[comb[2]].classList.contains("o"))
+      tabuleiro[comb[0]].classList.contains("x") &&
+      tabuleiro[comb[1]].classList.contains("x") &&
+      tabuleiro[comb[2]].classList.contains("x")
     ) {
-      toggleMensagem();
+      mensagemVencedor.innerHTML = "Jogador X venceu!";
+      placarX.innerHTML = parseInt(placarX.innerHTML) + 1;
+      abrirModal();
+      limparTabuleiro();
+    } else if (
+      tabuleiro[comb[0]].classList.contains("o") &&
+      tabuleiro[comb[1]].classList.contains("o") &&
+      tabuleiro[comb[2]].classList.contains("o")
+    ) {
+      mensagemVencedor.innerHTML = "Jogador O venceu!";
+      placarO.innerHTML = parseInt(placarO.innerHTML) + 1;
+      abrirModal();
       limparTabuleiro();
     }
   });
 }
 
-function toggleMensagem() {
-  if (modalNovoJogo.classList.contains("active")) {
-    modalNovoJogo.classList.remove("active");
-    modalNovoJogo.classList.add("hidden");
-  } else {
-    modalNovoJogo.classList.remove("hidden");
-    modalNovoJogo.classList.add("active");
+function mudarJogador() {
+  playerAtivo === "x" ? (playerAtivo = "o") : (playerAtivo = "x");
+}
+
+//checar se todas as casa do tabuleiro estao marcadas e retornar true
+function checkEmpate() {
+  let marcadas = 0;
+  tabuleiro.forEach((casa) => {
+    if (casa.classList.contains("marcado")) {
+      marcadas++;
+    }
+  });
+  if (marcadas === 9) {
+    mensagemVencedor.innerHTML = "Deu Velha!";
+    abrirModal();
+    limparTabuleiro();
   }
+}
+
+function abrirModal() {
+  modalNovoJogo.classList.remove("hidden");
+  modalNovoJogo.classList.add("active");
+}
+
+function jogarDeNovo() {
+  modalNovoJogo.classList.remove("active");
+  modalNovoJogo.classList.add("hidden");
+}
+
+function sair() {
+  placarO.innerHTML = 0;
+  placarX.innerHTML = 0;
+  jogarDeNovo();
 }
 
 function limparTabuleiro() {
